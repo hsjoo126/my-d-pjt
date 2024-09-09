ba24aacbf166a43ae67218a85d8aac68c7d1f270
hsjoo126 <hsjoo126@naver.com>
2024-09-09 12:47:54 +0900

    SECRET KEY 관리

--- my_d_pjt/settings.py
+++ my_d_pjt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from . import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rh0&w19q@&$^9h73*wyq$^jb&bs2w7n7efkw!v006k*!zt&^r$'
+SECRET_KEY = config.DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
